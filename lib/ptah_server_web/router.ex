a9ab45e9e95d414752e0db8d1cc81bc49ed5ac8f
Bohdan Shulha <b.shulha@pm.me>
2024-06-13 00:35:18 +0200

    feat: #27 add team_id parameter into all relevant urls

--- lib/ptah_server_web/router.ex
+++ lib/ptah_server_web/router.ex
@@ -1,7 +1,11 @@
 defmodule PtahServerWeb.Router do
+  require Logger
   use PtahServerWeb, :router
 
   import PtahServerWeb.UserAuth
+
+  import PtahServerWeb.Plug.CurrentTeam, only: [fetch_default_team: 2]
+  # import PtahServerWeb.Plug.CurrentTeam
 
   pipeline :browser do
     plug :accepts, ["html"]
@@ -12,6 +16,7 @@ defmodule PtahServerWeb.Router do
     plug :put_secure_browser_headers
     plug :fetch_current_user
     plug :analytics
+    plug :fetch_default_team
   end
 
   pipeline :api do
@@ -72,7 +77,10 @@ defmodule PtahServerWeb.Router do
     pipe_through [:browser, :redirect_if_user_is_authenticated]
 
     live_session :redirect_if_user_is_authenticated,
-      on_mount: [{PtahServerWeb.UserAuth, :redirect_if_user_is_authenticated}] do
+      on_mount: [
+        {PtahServerWeb.UserAuth, :redirect_if_user_is_authenticated},
+        {PtahServerWeb.Plug.CurrentTeam, :assign_nil_team}
+      ] do
       live "/users/register", UserRegistrationLive, :new
       live "/users/log_in", UserLoginLive, :new
       live "/users/reset_password", UserForgotPasswordLive, :new
@@ -85,52 +93,65 @@ defmodule PtahServerWeb.Router do
   scope "/", PtahServerWeb do
     pipe_through [:browser, :require_authenticated_user]
 
-    live_session :require_authenticated_user,
-      on_mount: [{PtahServerWeb.UserAuth, :ensure_authenticated}] do
+    live_session :user_portal,
+      on_mount: [
+        {PtahServerWeb.UserAuth, :ensure_authenticated},
+        {PtahServerWeb.Plug.CurrentTeam, :ensure_team_selection}
+      ] do
       live "/users/settings", UserSettingsLive, :edit
       live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+    end
 
-      live "/swarms", SwarmLive.Index, :index
-      live "/swarms/new", SwarmLive.Index, :new
-      live "/swarms/:id/edit", SwarmLive.Index, :edit
+    live_session :require_authenticated_user,
+      on_mount: [
+        {PtahServerWeb.UserAuth, :ensure_authenticated},
+        {PtahServerWeb.Plug.CurrentTeam, :ensure_team_access}
+      ] do
+      scope "/teams/:team_id" do
+        # pipe_through [:require_team_access]
 
-      live "/swarms/:id", SwarmLive.Show, :show
-      live "/swarms/:id/show/edit", SwarmLive.Show, :edit
+        live "/swarms", SwarmLive.Index, :index
+        live "/swarms/new", SwarmLive.Index, :new
+        live "/swarms/:id/edit", SwarmLive.Index, :edit
 
-      live "/servers", ServerLive.Index, :index
-      live "/servers/new", ServerLive.Index, :new
-      live "/servers/:id/edit", ServerLive.Index, :edit
+        live "/swarms/:id", SwarmLive.Show, :show
+        live "/swarms/:id/show/edit", SwarmLive.Show, :edit
 
-      live "/servers/:id", ServerLive.Show, :show
-      live "/servers/:id/show/edit", ServerLive.Show, :edit
+        live "/servers", ServerLive.Index, :index
+        live "/servers/new", ServerLive.Index, :new
+        live "/servers/:id/edit", ServerLive.Index, :edit
 
-      live "/stacks", StackLive.Index, :index
-      live "/stacks/new", StackLive.Index, :new
-      live "/stacks/:id/edit", StackLive.Index, :edit
+        live "/servers/:id", ServerLive.Show, :show
+        live "/servers/:id/show/edit", ServerLive.Show, :edit
 
-      live "/stacks/:id", StackLive.Show, :show
-      live "/stacks/:id/show/edit", StackLive.Show, :edit
+        live "/stacks", StackLive.Index, :index
+        live "/stacks/new", StackLive.Index, :new
+        live "/stacks/:id/edit", StackLive.Index, :edit
 
-      live "/services", ServiceLive.Index, :index
-      live "/services/new", ServiceLive.Index, :new
-      live "/services/:id/edit", ServiceLive.Index, :edit
+        live "/stacks/:id", StackLive.Show, :show
+        live "/stacks/:id/show/edit", StackLive.Show, :edit
 
-      live "/services/:id", ServiceLive.Show, :show
-      live "/services/:id/show/edit", ServiceLive.Show, :edit
+        live "/services", ServiceLive.Index, :index
+        live "/services/new", ServiceLive.Index, :new
+        live "/services/:id/edit", ServiceLive.Index, :edit
 
-      live "/docker_configs", DockerConfigLive.Index, :index
-      live "/docker_configs/new", DockerConfigLive.Index, :new
-      live "/docker_configs/:id/edit", DockerConfigLive.Index, :edit
+        live "/services/:id", ServiceLive.Show, :show
+        live "/services/:id/show/edit", ServiceLive.Show, :edit
 
-      live "/docker_configs/:id", DockerConfigLive.Show, :show
-      live "/docker_configs/:id/show/edit", DockerConfigLive.Show, :edit
+        live "/docker_configs", DockerConfigLive.Index, :index
+        live "/docker_configs/new", DockerConfigLive.Index, :new
+        live "/docker_configs/:id/edit", DockerConfigLive.Index, :edit
 
-      live "/docker_registries", DockerRegistryLive.Index, :index
-      live "/docker_registries/new", DockerRegistryLive.Index, :new
-      live "/docker_registries/:id/edit", DockerRegistryLive.Index, :edit
+        live "/docker_configs/:id", DockerConfigLive.Show, :show
+        live "/docker_configs/:id/show/edit", DockerConfigLive.Show, :edit
 
-      live "/docker_registries/:id", DockerRegistryLive.Show, :show
-      live "/docker_registries/:id/show/edit", DockerRegistryLive.Show, :edit
+        live "/docker_registries", DockerRegistryLive.Index, :index
+        live "/docker_registries/new", DockerRegistryLive.Index, :new
+        live "/docker_registries/:id/edit", DockerRegistryLive.Index, :edit
+
+        live "/docker_registries/:id", DockerRegistryLive.Show, :show
+        live "/docker_registries/:id/show/edit", DockerRegistryLive.Show, :edit
+      end
     end
   end
 
@@ -140,7 +161,10 @@ defmodule PtahServerWeb.Router do
     delete "/users/log_out", UserSessionController, :delete
 
     live_session :current_user,
-      on_mount: [{PtahServerWeb.UserAuth, :mount_current_user}] do
+      on_mount: [
+        {PtahServerWeb.UserAuth, :mount_current_user},
+        {PtahServerWeb.Plug.CurrentTeam, :assign_nil_team}
+      ] do
       live "/users/confirm/:token", UserConfirmationLive, :edit
       live "/users/confirm", UserConfirmationInstructionsLive, :new
     end
